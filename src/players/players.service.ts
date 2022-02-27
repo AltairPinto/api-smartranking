01c54b4eaa4f9af94dde726041540ab776cb1912
Altair Pinto <eng.altairpinto@gmail.com>
2022-02-27 15:43:25 -0300

    Get Player by email

--- src/players/players.service.ts
+++ src/players/players.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable, Logger, NotFoundException } from '@nestjs/common';
 import { CreatePlayerDto } from './dtos/create-player.dto';
 import { Player } from './interfaces/player.interface';
 import { v4 } from 'uuid';
@@ -22,6 +22,14 @@ export class PlayersService {
 
   async getAllPlayers(): Promise<Player[]> {
     return await this.players;
+  }
+
+  async getPlayerByEmail(email: string): Promise<Player> {
+    const findPlayer = this.players.find(
+      (player: Player) => player.email === email,
+    );
+    if (findPlayer) return findPlayer;
+    else throw new NotFoundException(`Player with email ${email} not found`);
   }
 
   private create(createPlayerDto: CreatePlayerDto): void {
