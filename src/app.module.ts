6026d595b38d21ad87d89de9f38f23e1c7786a09
Altair Pinto <eng.altairpinto@gmail.com>
2022-02-27 17:16:21 -0300

    dotenv config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,16 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PlayersModule } from './players/players.module';
+import 'dotenv/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://admin:vZNSOfNoEVDAlQv5@cluster0.cqdia.mongodb.net/smartRanking?retryWrites=true&w=majority',
-      {
-        useNewUrlParser: true,
-        // useCreateIndex: true,
-        useUnifiedTopology: true,
-        // useFindAndModify: true,
-      },
-    ),
+    MongooseModule.forRoot(process.env.APP_MONGOOSE, {
+      useNewUrlParser: true,
+      // useCreateIndex: true,
+      useUnifiedTopology: true,
+      // useFindAndModify: true,
+    }),
     PlayersModule,
   ],
   controllers: [],
